e52888e5989f080954b2ef10031c1874aaf2533a
Angelo <Angelo@LAPTOP-BTRPHQH5>
2024-02-26 16:48:23 +0800

    Add project files.

---
+++ Portfolio/Controllers/PortController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class PortController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+    }
+}
